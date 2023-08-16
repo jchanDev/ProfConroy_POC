fbb42f10d06ab855fe337bd726950e992c62faae
juchan2021 <60370842+juchan2021@users.noreply.github.com>
2023-08-16 10:39:36 -0700

    added error handling

--- POCLambda.py
+++ POCLambda.py
@@ -1,22 +1,28 @@
 import boto3
 
 def lambda_handler(event, context):
-    # Parse event data
-    s3_event = event['Records'][0]['s3']
-    bucket_name = s3_event['bucket']['name']
-    object_key = s3_event['object']['key']
+    try:
+        # Parse event data
+        s3_event = event['Records'][0]['s3']
+        bucket_name = s3_event['bucket']['name']
+        object_key = s3_event['object']['key']
 
-    # Specify the CloudFormation template URL
-    cloudformation_template_url = "https://lambda-store-bucket-poc-2023.s3.us-west-2.amazonaws.com/POCTemplate2AmazonLinux.yml"
+        # Specify the CloudFormation template URL
+        cloudformation_template_url = "https://lambda-store-bucket-poc-2023.s3.us-west-2.amazonaws.com/POCTemplate2AmazonLinux.yml"
 
-    # Launch CloudFormation stack
-    cloudformation_client = boto3.client('cloudformation')
-    response = cloudformation_client.create_stack(
-        StackName='POCTemplate2AmazonLinux',
-        TemplateURL=cloudformation_template_url,
-    )
+        # Launch CloudFormation stack
+        cloudformation_client = boto3.client('cloudformation')
+        response = cloudformation_client.create_stack(
+            StackName='POCTemplate2AmazonLinux',
+            TemplateURL=cloudformation_template_url,
+        )
 
-    return {
-        'statusCode': 200,
-        'body': 'Stack creation initiated'
-    }
+        return {
+            'statusCode': 200,
+            'body': 'Stack creation initiated'
+        }
+    except Exception as e:
+        return {
+            'statusCode': 500,
+            'body': f'Error: {str(e)}'
+        }
