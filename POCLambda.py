bab5ed247e15dccea8d4fdae0bb1c2303556412e
kpancha1 <keya020615@gmail.com>
2023-08-18 10:30:54 -0700

    back

--- POCLambda.py
+++ POCLambda.py
@@ -1,38 +1,46 @@
 import boto3
 
 def lambda_handler(event, context):
-    # Replace these values with your own
-    stack_name = 'POCTemplate2AmazonLinux'
-    template_url = 'https://lambda-store-bucket-poc-2023.s3.us-west-2.amazonaws.com/POCTemplate2AmazonLinux.yml'
     
-    cloudformation = boto3.client('cloudformation')
+    # Parse event data
+    s3_event = event['Records'][0]['s3']
+    print("s3event: " + s3_event)
+    bucket_name = s3_event['bucket']['name']
+    print("bucketname: " + bucket_name)
+    object_key = s3_event['object']['key']
+    print("objectkey: " + object_key)
+
+    # Specify the CloudFormation template URL
+    cloudformation_template_url = "https://lambda-store-bucket-poc-2023.s3.us-west-2.amazonaws.com/POCTemplate2AmazonLinux.yml"
+
+    # Launch CloudFormation stack
+    cloudformation_client = boto3.client('cloudformation')
+    response = cloudformation_client.create_stack(
+        StackName='POCTemplate2AmazonLinux',
+        TemplateURL=cloudformation_template_url,
+    )
+
+    return {
+        'statusCode': 200,
+        'body': 'Stack creation initiated'
+    }
+
     
-    try:
-        response = cloudformation.create_stack(
-            StackName=stack_name,
-            TemplateURL=template_url,
-            OnFailure='ROLLBACK'
-        )
-        
-        return {
-            'statusCode': 200,
-            'body': 'Stack creation initiated successfully'
-        }
-    except Exception as e:
-        s3 = boto3.client('s3')
+    # except Exception as e:
+    #     s3 = boto3.client('s3')
 
-        # Specify the S3 bucket and file path
-        bucket_name = 'csm-match-mock-data-matched'
-        file_key = 'error_log.txt'
+    #     # Specify the S3 bucket and file path
+    #     bucket_name = 'csm-match-mock-data-matched'
+    #     file_key = 'error_log.txt'
 
-        # Create the error message
-        error_message = f"Error: {str(e)}"
+    #     # Create the error message
+    #     error_message = f"Error: {str(e)}"
 
-        # Upload the error message as a text file to S3
-        s3.put_object(Bucket=bucket_name, Key=file_key, Body=error_message)
+    #     # Upload the error message as a text file to S3
+    #     s3.put_object(Bucket=bucket_name, Key=file_key, Body=error_message)
 
-        # Return a response indicating failure
-        return {
-            'statusCode': 500,
-            'body': 'An error occurred'
-        }
+    #     # Return a response indicating failure
+    #     return {
+    #         'statusCode': 500,
+    #         'body': 'An error occurred'
+    #     }
